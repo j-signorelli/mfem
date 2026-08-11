1ebd5780e1e1e1898e18a514af9bdfe0a319ceef
Joseph Signorelli <jms26@illinois.edu>
2026-08-11 11:48:30 -0500

    Use non-deprecated ctors

--- examples/ex18p.cpp
+++ examples/ex18p.cpp
@@ -204,7 +204,7 @@ int main(int argc, char *argv[])
    RusanovFlux numericalFlux(flux);
    DGHyperbolicConservationLaws euler(
       vfes, std::unique_ptr<HyperbolicFormIntegrator>(
-         new HyperbolicFormIntegrator(numericalFlux, IntOrderOffset)),
+         new HyperbolicFormIntegrator(flux, numericalFlux, IntOrderOffset)),
       preassembleWeakDiv);
 
    // 7. Visualize momentum with its magnitude
